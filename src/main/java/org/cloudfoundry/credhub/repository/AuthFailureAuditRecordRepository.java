2d1c14455db97de1b9ff6bf8c22538ae4a950459
Ben Moss <bmoss@pivotal.io>
2018-06-04 13:54:51 -0400

    Add transactional annotations for audit table clean up
    [#157432627] CredHub cleans up old audit table records
    
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- src/main/java/org/cloudfoundry/credhub/repository/AuthFailureAuditRecordRepository.java
+++ src/main/java/org/cloudfoundry/credhub/repository/AuthFailureAuditRecordRepository.java
@@ -2,9 +2,11 @@ package org.cloudfoundry.credhub.repository;
 
 import org.cloudfoundry.credhub.entity.AuthFailureAuditRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 
 public interface AuthFailureAuditRecordRepository extends JpaRepository<AuthFailureAuditRecord, Long> {
+  @Transactional
   void deleteByNowBefore(Instant expiryDate);
 }
