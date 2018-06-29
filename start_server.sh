8e1428203bc65eadb9a8dec9da4f223a0e240480
Chhavi Kankaria <ckankaria@pivotal.io>
2018-06-29 11:24:38 -0400

    Fix start_server script
    Signed-off-by: Anna Thornton <athornton@pivotal.io>

--- start_server.sh
+++ start_server.sh
@@ -2,5 +2,8 @@
 
 export VERSION=100.0.1
 
-./setup_dev_mtls.sh
-./gradlew --no-daemon bootRun -Djava.security.egd=file:/dev/urandom -Djdk.tls.ephemeralDHKeySize=3072 -Djdk.tls.namedGroups="secp384r1" -Djavax.net.ssl.trustStore=/Users/pivotal/workspace/credhub-release/src/credhub/src/test/resources/auth_server_trust_store.jks -Djavax.net.ssl.trustStorePassword=changeit $@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$DIR/setup_dev_mtls.sh
+$DIR/gradlew --no-daemon assemble
+exec $DIR/gradlew --no-daemon bootRun -Djava.security.egd=file:/dev/urandom -Djdk.tls.ephemeralDHKeySize=4096 -Djdk.tls.namedGroups="secp384r1" -Djavax.net.ssl.trustStore=src/test/resources/auth_server_trust_store.jks -Djavax.net.ssl.trustStorePassword=changeit $@
