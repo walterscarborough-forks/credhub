f8417ff3e793455f733574a32700a64817295ad8
Marty Spiewak <mspiewak@pivotal.io>
2018-10-15 10:52:42 -0400

    Removing certs from the right directory and removing build directory
    [#161118371]
    
    - fixes acceptance tests
    
    Signed-off-by: Victoria Henry <vhenry@pivotal.io>
    Signed-off-by: Marty Spiewak <mspiewak@pivotal.io>

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf ./build
 ./setup_dev_mtls.sh
 ./gradlew --no-daemon assemble
 ./gradlew --no-daemon bootRun -Djava.security.egd=file:/dev/urandom -Djdk.tls.ephemeralDHKeySize=4096 -Djdk.tls.namedGroups="secp384r1" -Djavax.net.ssl.trustStore=src/test/resources/auth_server_trust_store.jks -Djavax.net.ssl.trustStorePassword=changeit $@
